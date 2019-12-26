c8dc9a432d49d6e8abce5a7efc7c9852e9d91165
TK_User <khvostovets.kinoplan@gmail.com>
2019-12-26 16:57:53 +0300

    change the way models represented to json

--- app/models/Task.scala
+++ app/models/Task.scala
@@ -1,6 +1,6 @@
 package models
 
-import play.api.libs.json.{Json, OFormat}
+import play.api.libs.json.{Format, JsObject, JsResult, JsString, JsValue, Json, OFormat, OWrites, Reads}
 
 case class Task (
   id: Long,
@@ -10,6 +10,32 @@ case class Task (
   deleted: Boolean
 )
 
-object Task {
-  implicit val TaskFormat: OFormat[Task] = Json.format[Task]
+//object Task {
+//  implicit val TaskFormat: OFormat[Task] = Json.format[Task]
+//}
+
+object Task extends Format[Task] {
+
+  implicit val jsWrite: OWrites[Task] = Json.writes[Task]
+  implicit val jsRead: Reads[Task] = reads
+
+  override def reads(json: JsValue): JsResult[Task] =
+    for {
+      id <- (json \ "id").validate[String]
+      userId <- (json \ "userId").validate[String]
+      text <- (json \ "text").validate[String]
+      done <- (json \ "done").validate[Boolean]
+      deleted <- (json \ "deleted").validate[Boolean]
+    } yield
+      if (id.isEmpty) Task(0, userId.toLong, text, done, deleted)
+      else Task(id.toLong, userId.toLong, text, done, deleted)
+
+  override def writes(o: Task): JsValue = JsObject(List(
+    "id" -> JsString(o.id.toString),
+    "userId" -> JsString(o.userId.toString),
+    "text" -> JsString(o.text),
+    "done" -> JsString(o.done.toString),
+    "deleted" -> JsString(o.deleted.toString),
+  ))
+
 }
--- app/models/User.scala
+++ app/models/User.scala
@@ -1,6 +1,6 @@
 package models
 
-import play.api.libs.json.{Json, OFormat}
+import play.api.libs.json.{Format, JsObject, JsResult, JsString, JsValue, Json, OFormat, OWrites, Reads}
 
 case class User (
   id: Long,
@@ -8,6 +8,22 @@ case class User (
   password: String
 )
 
-object User {
-  implicit val TaskFormat: OFormat[User] = Json.format[User]
+object User extends Format[User] {
+
+  implicit val jsWrite: OWrites[User] = Json.writes[User]
+  implicit val jsRead: Reads[User] = reads
+
+  override def reads(json: JsValue): JsResult[User] =
+    for {
+      id <- (json \ "id").validate[String]
+      name <- (json \ "username").validate[String]
+      pass <- (json \ "password").validate[String]
+    } yield if (id.isEmpty) User(0, name, pass) else User(id.toLong, name, pass)
+
+  override def writes(o: User): JsValue = JsObject(List(
+    "id" -> JsString(o.id.toString),
+    "username" -> JsString(o.username),
+    "password" -> JsString(o.password)
+  ))
+
 }
